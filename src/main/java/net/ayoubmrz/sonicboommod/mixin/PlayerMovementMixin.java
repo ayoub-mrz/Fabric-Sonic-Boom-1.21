bac13d636233433d93510a474276b19cfad31b80
ayoub-mrz <ayoub555merizak@gmail.com>
2025-08-30 09:23:53 +0100

    making Elge Statue obtained only one time, adjusting numberToBoom to 30

--- src/main/java/net/ayoubmrz/sonicboommod/mixin/PlayerMovementMixin.java
+++ src/main/java/net/ayoubmrz/sonicboommod/mixin/PlayerMovementMixin.java
@@ -2,14 +2,13 @@ package net.ayoubmrz.sonicboommod.mixin;
 
 import net.ayoubmrz.sonicboommod.block.ModBlocks;
 import net.ayoubmrz.sonicboommod.particle.ModParticles;
-import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.advancement.AdvancementProgress;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
@@ -24,11 +23,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerEntity.class)
 public class PlayerMovementMixin {
 	private Vec3d lastPosition = Vec3d.ZERO;
-	private int numberToBoom = 20;
+	private int numberToBoom = 30;
 	private boolean sonicBoom = false;
 	private boolean timerStart = false;
 	private int timer = 0;
+	private boolean hasGotItem = false;
 
+	private static final String SONIC_BOOM_DATA_KEY = "SonicBoomMod";
+	private static final String HAS_GOT_ITEM_KEY = "hasGotItem";
 
 	@Inject(method = "tick", at = @At("HEAD"))
 	private void onPlayerTick(CallbackInfo ci) {
@@ -39,9 +41,8 @@ public class PlayerMovementMixin {
 			double speed = currentPos.distanceTo(lastPosition);
 			ItemStack chestItem = player.getEquippedStack(EquipmentSlot.CHEST);
 
-			if (speed > 1.7 && !sonicBoom && chestItem.isOf(Items.ELYTRA)) {
+			if (speed > 1.72 && !sonicBoom && chestItem.isOf(Items.ELYTRA)) {
 				numberToBoom--;
-
 			}
 
 			if (timerStart) {
@@ -74,13 +75,18 @@ public class PlayerMovementMixin {
 							1, 0.0, 0.0, 0.0, 0.0
 					);
 
-					ItemStack stack = new ItemStack(ModBlocks.EGLE_STATUE, 1);
-					boolean inserted = player.getInventory().insertStack(stack);
+					if (!hasGotItem) {
+						ItemStack stack = new ItemStack(ModBlocks.EGLE_STATUE, 1);
+						boolean inserted = player.getInventory().insertStack(stack);
 
-					if (!inserted) {
-						player.dropItem(stack, false);
+						if (!inserted) {
+							player.dropItem(stack, false);
+						}
+						hasGotItem = true;
+
+						// Save the hasGotItem state to NBT
+						saveHasGotItemToNBT(player);
 					}
-
 				}
 				sonicBoom = true;
 				timerStart = true;
@@ -99,11 +105,30 @@ public class PlayerMovementMixin {
 						}
 					}
 				}
-
 			}
-
 		}
 
 		lastPosition = currentPos;
+	}
+
+	@Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
+	private void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo ci) {
+		if (nbt.contains(SONIC_BOOM_DATA_KEY)) {
+			NbtCompound sonicBoomData = nbt.getCompound(SONIC_BOOM_DATA_KEY);
+			this.hasGotItem = sonicBoomData.getBoolean(HAS_GOT_ITEM_KEY);
+		}
+	}
+
+	@Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
+	private void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo ci) {
+		NbtCompound sonicBoomData = new NbtCompound();
+		sonicBoomData.putBoolean(HAS_GOT_ITEM_KEY, this.hasGotItem);
+		nbt.put(SONIC_BOOM_DATA_KEY, sonicBoomData);
+	}
+
+	private void saveHasGotItemToNBT(PlayerEntity player) {
+		if (player instanceof ServerPlayerEntity serverPlayer) {
+			serverPlayer.getServer().getPlayerManager().loadPlayerData(serverPlayer);
+		}
 	}
 }

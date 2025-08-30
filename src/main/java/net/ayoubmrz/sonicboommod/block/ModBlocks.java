ffbb8db81cd900a6f05897effee213a8e360bfd4
ayoub-mrz <ayoub555merizak@gmail.com>
2025-08-30 12:52:21 +0100

    Update to 1.21.3

--- src/main/java/net/ayoubmrz/sonicboommod/block/ModBlocks.java
+++ src/main/java/net/ayoubmrz/sonicboommod/block/ModBlocks.java
@@ -8,22 +8,28 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+
+import java.util.function.Function;
 
 public class ModBlocks {
 
     public static final Block EGLE_STATUE = registerBlock("egle_statue",
-            new EgleStatueBlock(AbstractBlock.Settings.create().nonOpaque()));
+            properties -> new EgleStatueBlock(properties.nonOpaque()));
 
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(SonicBoomMod.MOD_ID, name), block);
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> function) {
+        Block toRegister = function.apply(AbstractBlock.Settings.create().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(SonicBoomMod.MOD_ID, name))));
+        registerBlockItem(name, toRegister);
+        return Registry.register(Registries.BLOCK, Identifier.of(SonicBoomMod.MOD_ID, name), toRegister);
     }
 
     private static void registerBlockItem(String name, Block block) {
         Registry.register(Registries.ITEM, Identifier.of(SonicBoomMod.MOD_ID, name),
-                new BlockItem(block, new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
+                new BlockItem(block, new Item.Settings().useBlockPrefixedTranslationKey()
+                        .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SonicBoomMod.MOD_ID, name)))));
     }
 
     public static void registerModBlocks() {
